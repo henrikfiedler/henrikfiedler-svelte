f6fcfa159552e6f150dd68c98b023047788f02de
henrikfiedler <33574047+henrikfiedler@users.noreply.github.com>
2023-01-30 17:08:11 +0100

    remove Navbar Test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,31 +27,7 @@
 	let isOpen = false;
 </script>
 
-<Navbar color="light" light expand="md">
-	<NavbarBrand href="/">sveltestrap</NavbarBrand>
-	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
-	<Collapse {isOpen} navbar expand="md" on:update={() => (isOpen = !isOpen)}>
-		<Nav class="ms-auto" navbar>
-			<NavItem>
-				<NavLink href="#components/">Components</NavLink>
-			</NavItem>
-			<NavItem>
-				<NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
-			</NavItem>
-			<Dropdown nav inNavbar>
-				<DropdownToggle nav caret>Options</DropdownToggle>
-				<DropdownMenu end>
-					<DropdownItem>Option 1</DropdownItem>
-					<DropdownItem>Option 2</DropdownItem>
-					<DropdownItem divider />
-					<DropdownItem>Reset</DropdownItem>
-				</DropdownMenu>
-			</Dropdown>
-		</Nav>
-	</Collapse>
-</Navbar>
-
-<!-- <header>
+<header>
 	<nav id="nav" class="navbar navbar-expand navbar-dark fixed-top">
 		<div class="container">
 			<a class="navbar-brand" href="/">
@@ -67,7 +43,7 @@
 			</a>
 		</div>
 	</nav>
-</header> -->
+</header>
 
 <main class="flex-grow-1">
 	<slot />
