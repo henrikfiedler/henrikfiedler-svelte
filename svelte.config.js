f54c6ea650a4e408da66abe42a19c0ec63e56fc4
henrikfiedler <33574047+henrikfiedler@users.noreply.github.com>
2024-04-27 16:57:26 +0200

    update vite-preprocess in svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,14 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
-		vitePreprocess(),
+		vitePreprocess({}),
 		preprocess({
 			scss: {
 				prependData: '@use "src/variables.scss" as *;'
